1dc4eee2520d75ff068bd0358c3b777855771d64
ANJOON_JEONG <40282272+JungAnJoon@users.noreply.github.com>
2022-06-21 21:42:22 +0900

    customer.java update

--- CustomerTeam/src/main/java/msalogin/domain/Customer.java
+++ CustomerTeam/src/main/java/msalogin/domain/Customer.java
@@ -17,11 +17,11 @@ import org.springframework.beans.BeanUtils;
 
 import javax.persistence.*;
 
-@Getter
-@Setter
+// @Getter
+// @Setter
 @Entity
 @Table(name = "CustomerList_Base")
-@Data
+// @Data
 public class Customer {
 
     @Id
@@ -37,23 +37,90 @@ public class Customer {
     @Column
     private String JuminNo;
 
-    @PostPersist
+       @PostPersist
     public void onPostPersist() {
         CustomerRegistered customerRegistered = new CustomerRegistered();
         BeanUtils.copyProperties(this, customerRegistered);
         customerRegistered.publishAfterCommit();
 
-        CustomerCancelled customerCancelled = new CustomerCancelled();
-        BeanUtils.copyProperties(this, customerCancelled);
-        customerCancelled.publishAfterCommit();
+        //Following code causes dependency to external APIs
+        // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
 
+//         fooddelivery.external.Payment payment = new fooddelivery.external.Payment();
+//         // mappings goes here
+//         FrontApplication.applicationContext
+//             .getBean(fooddelivery.external.PaymentService.class)
+//             .pay(payment);
     }
 
-    public static CustomerRepository repository() {
-        CustomerRepository customerRepository = CustomerTeamApplication.applicationContext.getBean(
-            CustomerRepository.class
-        );
-        return customerRepository;
+    @PostRemove
+    public void onPostRemove() {
+        CustomerCancelled customerCanceled = new CustomerCanceled();
+        BeanUtils.copyProperties(this, customerCanceled);
+        customerCanceled.publishAfterCommit();
     }
-    
+
+    @PrePersist
+    public void onPrePersist() {}
+
+    @PreRemove
+    public void onPreRemove() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFoodId() {
+        return foodId;
+    }
+
+    public void setFoodId(String foodId) {
+        this.foodId = foodId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getPreference() {
+        return preference;
+    }
+
+    public void setPreference(String preference) {
+        this.preference = preference;
+    }
+
+    public String getOptions() {
+        return options;
+    }
+
+    public void setOptions(String options) {
+        this.options = options;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
+
+
