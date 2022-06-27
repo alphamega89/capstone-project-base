be5445b21c4c738f9311762c9cead3bd18007e72
DevJihwan <jh552016@naver.com>
2022-06-27 02:47:58 +0000

    commint on main

--- Login/src/main/java/msalogin/domain/LoginService.java
+++ Login/src/main/java/msalogin/domain/LoginService.java
@@ -1,5 +1,5 @@
 package msalogin.domain;
 
 public interface LoginService {
-    void loginValidate (int customerId, String bankingId, String password);
+    void loginValidate (String bankingId, String password);
 }
